f2460ad8641a4a66e65133ba222adfa79eb0b3eb
bhanuchaitanya123 <142810475+bhanuchaitanya123@users.noreply.github.com>
2023-09-15 11:06:15 +0530

    initial commit

---
+++ vid2.py
@@ -0,0 +1,46 @@
+from vidstream import *
+import tkinter as tk
+import socket
+import threading
+local_ip_address=socket.gethostbyname(socket.gethostname())
+
+server=StreamingServer(local_ip_address,7777)
+receiver=AudioReceiver(local_ip_address,6666)
+window=tk.Tk()
+window.title("hello")
+window.geometry("500x200")
+
+label_target_ip=tk.Label(window,text="target ip:")
+label_target_ip.pack()
+text_target_ip=tk.Text(window,height=1)
+text_target_ip.pack()
+def start_listening():
+    t1=threading.Thread(target=server.start_server)
+    t2=threading.Thread(target=receiver.start_server)
+    t1.start()
+    t2.start()
+def start_camera():
+    camera_client=CameraClient(text_target_ip.get(1.0,'end-1c'),9999)
+    t3=threading.Thread(target=camera_client.start_stream)
+    t3.start()
+def start_screen():
+    screen_client=ScreenShareClient(text_target_ip.get(1.0,'end-1c'),9999)
+    t4=threading.Thread(target=screen_client.start_stream)
+    t4.start()  
+def start_audio():
+    audio_client=AudioSender(text_target_ip.get(1.0,'end-1c'),8888)
+    t5=threading.Thread(target=audio_client.start_stream)
+    t5.start()
+btnl=tk.Button(window,text="start camera",width=50,command=start_listening)
+btnl.pack(anchor=tk.CENTER,expand=True)
+
+btnc=tk.Button(window,text="start camera",width=50,command=start_camera)
+btnc.pack(anchor=tk.CENTER,expand=True)
+
+btns=tk.Button(window,text="start screen sharing",width=50,command=start_screen)
+btns.pack(anchor=tk.CENTER,expand=True)
+
+btna=tk.Button(window,text="start audio",width=50,command=start_audio)
+btna.pack(anchor=tk.CENTER,expand=True)
+
+window.mainloop()
